cc2893d42b3417647dfc8942c27c0c068628724f
raviolin <raviolin@icloud.com>
2018-06-05 16:28:09 +0900

    0.1.0

--- services/index.js
+++ services/index.js
@@ -1,83 +1,85 @@
 const moment = require('moment');
+const rmisjs = require('rmisjs');
+
 const birthFormat = t => moment(t).format('YYYY-MM-DD');
-module.exports = c => {
-    const rmisjs = require('rmisjs')(c);
-    const composer = rmisjs.composer;
+const consoleOutput = args => console.log('args > ', args);
+
+module.exports = s => {
+    const { composer } = rmisjs(s);
+    const connection = composer.mongoConnect();
+
+    const changeSlotState = async (args, cb, headers, req) => {
+        let data = {
+            birthDate: birthFormat(args.slotInfo.patientInfo.birthDate),
+            searchDocument: {
+                docTypeId: 26,
+                docNumber: args.slotInfo.patientInfo.policyNumber
+            },
+            GUID: args.slotInfo.GUID
+        };
+        await connection;
+        if (args.status && parseInt(args.status) === 3) {
+            try {
+                let r = await composer.deleteVisit(data);
+                if (r.length > 0) {
+                    cb({ 'lt:slipNumber': r });
+                } else {
+                    cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка отмены записи на прием' } });
+                }
+            } catch (e) {
+                console.log(e);
+                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Попробуйте отменить запись позже' } });
+            }
+        } else if (parseInt(args.status) !== 3) {
+            try {
+                let r = await composer.createVisit(data);
+                if (r.length > 0) {
+                    cb({ 'lt:slipNumber': r });
+                } else {
+                    cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка записи на прием' } });
+                }
+            } catch (e) {
+                console.log(e);
+                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка записи на прием' } });
+            }
+        } else {
+            cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка изменения состояния записи' } });
+        }
+    };
+
+    const validatePatient = async (args, cb, headers, req) => {
+        let data = {
+            birthDate: birthFormat(args.patientInfo.birthDate),
+            searchDocument: {
+                docTypeId: 26,
+                docNumber: args.patientInfo.policyNumber
+            }
+        };
+        await connection;
+        try {
+            let r = await composer.validatePatient(data);
+            if (r && r.response && r.response.statusCode === 500) {
+                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Нет прикрепления к учреждению' } });
+            } else if (r && !r.response) {
+                console.log(r);
+                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 0 } });
+            } else {
+                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Не найден в базе учреждения' } });
+            }
+        } catch (e) {
+            console.log(e);
+            cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка прикрепления к учреждению' } });
+        }
+    };
+
     return {
         service: {
             port: {
-                changeSlotState: (args, cb, headers, req) => {
-                    let data = {
-                        birthDate: birthFormat(args.slotInfo.patientInfo.birthDate),
-                        searchDocument: {
-                            docTypeId: 26,
-                            docNumber: args.slotInfo.patientInfo.policyNumber
-                        },
-                        GUID: args.slotInfo.GUID
-                    };
-                    if (args.status && parseInt(args.status) === 3) {
-                        composer.deleteVisit(data)
-                            .then(r => {
-                                if (r.length > 0) {
-                                    cb({ 'lt:slipNumber': r });
-                                } else {
-                                    cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка отмены записи на прием' } });
-                                }
-                            })
-                            .catch(e => {
-                                console.log(e);
-                                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Попробуйте отменить запись позже' } });
-                            });
-                    } else if (parseInt(args.status) !== 3) {
-                        composer.createVisit(data)
-                            .then(r => {
-                                if (r.length > 0) {
-                                    cb({ 'lt:slipNumber': r });
-                                } else {
-                                    cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка записи на прием' } });
-                                }
-                            })
-                            .catch(e => {
-                                console.log(e);
-                                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка записи на прием' } });
-                            });
-                    } else {
-                        cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка изменения состояния записи' } });
-                    }
-                },
-                checkPatient: args => {
-                    console.log('args > ', args);
-                },
-                validatePatient: (args, cb, headers, req) => {
-                    let data = {
-                        birthDate: birthFormat(args.patientInfo.birthDate),
-                        searchDocument: {
-                            docTypeId: 26,
-                            docNumber: args.patientInfo.policyNumber
-                        }
-                    };
-                    composer.validatePatient(data)
-                        .then(r => {
-                            if (r && r.response && r.response.statusCode === 500) {
-                                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Нет прикрепления к учреждению' } });
-                            } else if (r && !r.response) {
-                                console.log(r);
-                                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 0 } });
-                            } else {
-                                cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Не найден в базе учреждения' } });
-                            }
-                        })
-                        .catch(e => {
-                            console.log(e);
-                            cb({ 'ct:portalServiceResponse': { 'ct:ErrorCode': 1, 'ct:ErrorText': 'Ошибка прикрепления к учреждению' } });
-                        });
-                },
-                putParameters: args => {
-                    console.log('args > ', args);
-                },
-                getAttachedMO: args => {
-                    console.log('args > ', args);
-                }
+                changeSlotState,
+                validatePatient,
+                checkPatient: consoleOutput,
+                putParameters: consoleOutput,
+                getAttachedMO: consoleOutput
             }
         }
     };
